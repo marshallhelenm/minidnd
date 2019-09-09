804851b2bbf34704659be34257423fce6c9ec7bb
marshallhelenm <marshallhelenm@gmail.com>
2019-09-09 15:33:13 -0700

    yup

--- mini-dnd-backend/db/migrate/20190907162423_create_characters.rb
+++ mini-dnd-backend/db/migrate/20190907162423_create_characters.rb
@@ -1,19 +1,21 @@
 class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
+      t.string :name
       t.references :user, foreign_key: true
       t.references :class_type, foreign_key: true
       t.references :race, foreign_key: true
-      t.string :name
-      t.integer :max_hp
-      t.integer :hp
-      t.integer :armor
       t.string :weapon
+      t.string :armor
+      #calculated things:
+      t.integer :armor_class
       t.integer :athletics
       t.integer :subterfuge
       t.integer :lore
       t.integer :physical_save
       t.integer :magic_save
+      t.integer :max_hp
+      t.integer :hp
       t.integer :level
 
       t.timestamps
